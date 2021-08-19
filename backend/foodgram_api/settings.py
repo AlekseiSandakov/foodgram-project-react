e8fdef7b7cc69b973f0cbb7ea186227b0e715d33
AlekseiSandakov <axe455990@mail.ru>
2021-08-19 23:19:54 +0700

    my 19 commit

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'DEFAULT_PARAM')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['178.154.203.39', 'localhost', 'web',]
+ALLOWED_HOSTS = ['84.252.130.70', 'localhost', 'web',]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
