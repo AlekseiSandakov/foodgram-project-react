a9c6fe6f489ca4bac1e27b5dda9757314f8e26d8
AlekseiSandakov <axe455990@mail.ru>
2021-08-27 17:45:51 +0700

    my 96 commit

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -12,7 +12,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'DEFAULT_PARAM')
 
-
 DEBUG = False
 
 ALLOWED_HOSTS = ['178.154.220.225', 'localhost', 'backend',]
